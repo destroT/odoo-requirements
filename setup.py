bedf6b7f6729fa607dd112c1c9e98ab79ca89a3a
destroT <destro.tommaso@gmail.com>
2021-02-03 14:49:53 +0100

    Change url repo

--- setup.py
+++ setup.py
@@ -3,21 +3,23 @@ from setuptools import setup, find_packages
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
+
 def read_requirements():
-    with open('requirements.txt') as req:
+    with open("requirements.txt") as req:
         content = req.read()
-        requirements = content.split('\n')
+        requirements = content.split("\n")
     return requirements
 
+
 setup(
-    name="odoorequirements-mr_right", # Replace with your own username
+    name="odoo-req",  # Replace with your own username
     version="0.0.1",
     author="Tommaso Destro",
     author_email="destro.tommaso@gmail.com",
     description="A simple requirements generator for odoo modules",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/destroT/odoo-requirements",
     packages=find_packages(),
     include_package_data=True,
     install_requires=read_requirements(),
@@ -26,9 +28,9 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
-    entry_points='''
+    python_requires=">=3.6",
+    entry_points="""
         [console_scripts]
         odooreq=odooreq.cli:cli
-    '''
+    """,
 )
